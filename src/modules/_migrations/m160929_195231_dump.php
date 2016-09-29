ae55989817ccf428d37a06364f93893d40cb7c24
Tobias Munk <schmunk@usrbin.de>
2016-09-29 23:18:39 +0200

    added demo-data

---
+++ src/modules/_migrations/m160929_195231_dump.php
@@ -0,0 +1,9 @@
+<?php
+
+use dmstr\db\mysql\FileMigration;
+
+class m160929_195231_dump extends FileMigration
+{
+    # create a sql file `m160929_195231_dump.sql` or adjust and uncomment the following line, do not change this class name
+    //public $file = 'custom-filename.sql';
+}
---
+++ src/modules/_migrations/m160929_195231_dump.sql
@@ -0,0 +1,6 @@
+INSERT INTO `app_settings` (`id`, `type`, `section`, `key`, `value`, `active`, `created`, `modified`)
+VALUES
+	(3, 'string', 'docs', 'markdownUrl', 'https://raw.githubusercontent.com/phundament/docs/master', 1, '2016-01-08 16:29:35', '2016-09-29 02:45:33'),
+	(4, 'string', 'docs', 'defaultIndexFile', '1-introduction/about.md', 1, '2016-01-08 16:30:35', '2016-09-29 02:45:42'),
+	(5, 'string', 'docs', 'forkUrl', 'https://github.com/phundament/docs/edit/master', 1, '2016-01-08 16:31:30', '2016-09-29 02:45:48'),
+	(6, 'string', 'docs', 'htmlUrl', 'http://docs.phundament.com/5.0.0-beta3/', 1, '2016-09-29 02:46:55', NULL);
